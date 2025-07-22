e521bab20cd95e56c81f70e7eccc33009ced7571
Emmaculate Mwania <Kamauemmaculate0@gmail.com>
2025-07-22 18:22:02 +0300

    Removed render-build.sh and updated Render setttings

--- render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# render-build.sh
-cd server
-
-# (Optional) Run build if you ever add a build script
-pnpm run build || echo "No build step defined"
-
-echo "Build completed"
