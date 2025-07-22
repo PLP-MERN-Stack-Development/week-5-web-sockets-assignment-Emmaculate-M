4cd9c5806ce4ea3cf7139dcaafd703f741ddbecd
Emmaculate Mwania <Kamauemmaculate0@gmail.com>
2025-07-22 17:46:29 +0300

    Fix Render Deployment

--- render-build.sh
+++ render-build.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
 # render-build.sh
-# Install dependencies and skip optional peer warnings
 cd server
-pnpm install
 
 # (Optional) Run build if you ever add a build script
 pnpm run build || echo "No build step defined, continuing..."
